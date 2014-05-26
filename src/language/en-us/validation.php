c14f467f14f38c0ab435dc0bc495296cea461242
Mario Döring <deluna.yy@gmail.com>
2014-05-26 22:46:40 +0200

    added more validator messages

--- src/language/en-us/validation.php
+++ src/language/en-us/validation.php
@@ -2,6 +2,8 @@
 	'required' => 'The :field field is required.',
 	'email' => 'The :field format is invalid.',
 	'min' => 'The :field must have at least :0 characters.',
+	'max' => 'The :field cannot have more than :0 characters',
+	'between' => 'The :field should be between :0 and :1 characters.',
 	'match' => 'The :field field must match the :0 field.',
 	'negative' => 'The :field must be negative.',
 );
