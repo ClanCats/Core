e1c2a9c783c16477a7e411562216ff1bce9d6a4e
Mario Döring <deluna.yy@gmail.com>
2014-07-13 17:05:13 +0200

    loop view tests

--- tests/Core/CCView/Builder.php
+++ tests/Core/CCView/Builder.php
@@ -27,4 +27,48 @@ class CCView_Builder_Test extends \PHPUnit_Framework_TestCase
 	
 		$this->assertEquals( $expected_output, $output );
 	}
+	
+	/**
+	 * tests Builder loop 
+	 */
+	public function test_loop()
+	{
+		$output = CCView::create( 'CCUnit::builder/loop.view' )->render();
+	
+		$expected_output = "0123456789";
+	
+		$this->assertEquals( $expected_output, $output );
+	}
+	
+	/**
+	 * tests Builder loop 
+	 */
+	public function test_each()
+	{
+		$output = CCView::create( 'CCUnit::builder/each.view', array(
+			'items' => array( 'foo', 'bar', 'jep' ),
+		))->render();
+	
+		$expected_output = "foo, bar, jep, ";
+	
+		$this->assertEquals( $expected_output, $output );
+	}
+	
+	/**
+	 * tests Builder loop 
+	 */
+	public function test_array_access()
+	{
+		$output = CCView::create( 'CCUnit::builder/each_user.view', array(
+			'users' => array( 
+				array( 'name' => 'jeff' ),
+				array( 'name' => 'john' ),
+				array( 'name' => 'jack' ),
+			),
+		))->render();
+	
+		$expected_output = "<ul>\n\t<li>jeff</li>\n\t<li>john</li>\n\t<li>jack</li>\n</ul>";
+	
+		$this->assertEquals( $expected_output, $output );
+	}
 }
