aa0de7d1c203ad0266e4826cc6499867322514f8
Mario Döring <deluna.yy@gmail.com>
2014-08-02 13:05:43 +0200

    Fixed array compiler outside php tags

--- src/classes/CCView/Builder/CCFTemplate.php
+++ src/classes/CCView/Builder/CCFTemplate.php
@@ -17,7 +17,7 @@ class CCView_Builder_CCFTemplate implements CCView_Builder_Interface
 	 * @var string
 	 */
 	protected $content = null;
-	
+
 	/**
 	 * Starting commands
 	 *
@@ -32,7 +32,7 @@ class CCView_Builder_CCFTemplate implements CCView_Builder_Interface
 		'loop',
 		'switch',
 	);
-	
+
 	/**
 	 * Ending commands
 	 *
@@ -48,7 +48,7 @@ class CCView_Builder_CCFTemplate implements CCView_Builder_Interface
 		'continue',
 		'endswitch',
 	);
-	
+
 	/**
 	 * Continue commands
 	 *
@@ -57,7 +57,7 @@ class CCView_Builder_CCFTemplate implements CCView_Builder_Interface
 	protected $bracket_continue_commands = array(
 		'else',
 	);
-	
+
 	/**
 	 * View builder contructor
 	 *
@@ -68,7 +68,7 @@ class CCView_Builder_CCFTemplate implements CCView_Builder_Interface
 	{
 		$this->content = $content;
 	}
-	
+
 	/**
 	 * Compile method returns the compiled php view file
 	 *
@@ -79,12 +79,12 @@ class CCView_Builder_CCFTemplate implements CCView_Builder_Interface
 		$this->transform( 'echos' );
 		$this->transform( 'phptag' );
 		$this->transform( 'arrays' );
-		
+
 		//_dd( $this->content );
-		
+
 		return $this->content;
 	}
-	
+
 	/**
 	 * Execute a transformation
 	 *
@@ -95,20 +95,20 @@ class CCView_Builder_CCFTemplate implements CCView_Builder_Interface
 	{
 		$this->content = call_user_func( array( $this, 'compile_'.$function ), $this->content );
 	}
-	
+
 	/**
 	 * Repair an expression
 	 */
 	public function repair_expression( $exp )
 	{
 		$commands = explode( ' ', $exp );		
-		
+
 		// filter empty ones
 		$commands = array_filter( $commands, function( $value ) 
 		{
 			return !is_null( $value );
 		});
-		
+
 		// bracket starting command
 		if ( in_array( $commands[0], $this->bracket_starting_commands ) )
 		{
@@ -123,31 +123,31 @@ class CCView_Builder_CCFTemplate implements CCView_Builder_Interface
 				$commands[0] = 'for';
 				$commands[1] = '$i=0;$i<'.$commands[1].';$i++';
 			}
-			
+
 			// remove the opening duble point
 			end($commands); $key = key($commands);
 			if ( substr( $commands[$key], -1 ) == ':' )
 			{
 				$commands[$key] = substr( $commands[$key], 0, -1 );
-				
+
 				// is it now empty?
 				if ( $commands[$key] == ' ' || empty( $commands[$key] ) )
 				{
 					unset( $commands[$key] );
 				}
 			}
-			
+
 			// do we have brackets?
 			if ( substr( $commands[1], 0, 1 ) != '(' )
 			{
 				// add starting bracket
 				$commands[1] = '( '.$commands[1];
-				
+
 				// add ending bracket
 				end($commands); $key = key($commands);
 				$commands[$key] .= ' )';
 			}
-			
+
 			$commands[] = ':';
 		}
 		// bracket ending command
@@ -163,7 +163,7 @@ class CCView_Builder_CCFTemplate implements CCView_Builder_Interface
 			{
 				$commands[0] = 'endfor';
 			}
-			
+
 			// check for semicolon
 			if ( substr( $commands[0], 0, 1 ) != ';' )
 			{
@@ -178,21 +178,21 @@ class CCView_Builder_CCFTemplate implements CCView_Builder_Interface
 			if ( substr( $commands[$key], -1 ) == ':' )
 			{
 				$commands[$key] = substr( $commands[$key], 0, -1 );
-				
+
 				// is it now empty?
 				if ( $commands[$key] == ' ' || empty( $commands[$key] ) )
 				{
 					unset( $commands[$key] );
 				}
 			}
-			
+
 			// add the double point
 			$commands[] = ':';
 		}
-		
+
 		return implode( ' ', $commands );
 	}
-	
+
 	/**
 	 * Search and replace the echo commands shortcuts
 	 *
@@ -203,17 +203,17 @@ class CCView_Builder_CCFTemplate implements CCView_Builder_Interface
 		return preg_replace_callback('/\{\{(.*?)\}\}/s', function( $match )
 		{ 
 			$print = trim( $match[1] );
-			
+
 			// add missing semicolons
 			if ( substr( $print, -1 ) != ';' )
 			{
 				$print .= ';';
 			}
-			
+
 			return '<?php echo '.$print.' ?>'; 
 		}, $view );
 	}
-	
+
 	/**
 	 * Search and replace for shortcuts of the php tag
 	 *
@@ -224,18 +224,18 @@ class CCView_Builder_CCFTemplate implements CCView_Builder_Interface
 	{
 		// I hate this workaround
 		$that = $this;
-		
+
 		return preg_replace_callback('/\{\%(.*?)\%\}/s', function( $match ) use( $that )
 		{ 
 			$expression = trim( $match[1] );
-			
+
 			// repair it 
 			$expression = $that->repair_expression( $expression );
-			
+
 			return '<?php '.$expression.' ?>'; 
 		}, $view );
 	}
-	
+
 	/**
 	 * Search and replace vars with . array access
 	 *
@@ -243,59 +243,101 @@ class CCView_Builder_CCFTemplate implements CCView_Builder_Interface
 	 * @return void
 	 */
 	private function compile_arrays( $view )
-	{
-		// I hate this workaround
-		$that = $this;
-		
-		return preg_replace_callback('/(\$[^)\s]+)/s', function( $match ) use( $that )
-		{ 
-			$var = $match[1];
-			
-			if ( strpos( $var, '.' ) !== false )
+	{	
+		$tokens = token_get_all( $view );
+
+		$tags = array( 0 => '' );
+		$tag_index = 0;
+		$in_tag = false;
+
+		// parse all php tags out of the view
+		foreach ( $tokens as $token ) 
+		{
+			if ( is_array( $token ) ) 
 			{
-				$buffer = '';
-				$length = strlen( $var );
-				$inside_arr = false;
-				
-				for( $i=0;$i<$length;$i++ )
+				if ( $token[0] === T_OPEN_TAG )
 				{
-					$char = $var[$i];
-					
-					if ( $char == '.' && !$inside_arr )
+					$in_tag = true;
+				}
+
+				if ( $in_tag && !in_array( $token[0], array( T_INLINE_HTML ) ) ) 
+				{				
+					$tags[$tag_index] .= $token[1];
+				}
+
+				if ( $token[0] === T_CLOSE_TAG )
+				{
+					$in_tag = false;
+					$tag_index++;
+				}
+			}
+			else 
+			{
+				if ( $in_tag )
+				{
+					$tags[$tag_index] .= $token;
+				}
+			}
+		}
+
+		// lets make the tags search keys
+		$tags = array_flip( $tags );
+
+		// now search and replace var in the php sections
+		foreach( $tags as $search => &$replace )
+		{
+			$replace = preg_replace_callback('/(\$[^)\s]+)/s', function( $match )
+			{ 
+				$var = $match[1];
+
+				if ( strpos( $var, '.' ) !== false )
+				{
+					$buffer = '';
+					$length = strlen( $var );
+					$inside_arr = false;
+
+					for( $i=0;$i<$length;$i++ )
 					{
-						$buffer .= "['";
-						$inside_arr = true;
-					}
-					else
-					{
-						if ( $inside_arr && in_array( $char, array( ';', '-' ) ) )
+						$char = $var[$i];
+
+						if ( $char == '.' && !$inside_arr )
 						{
-							$buffer .= "']";
-							$inside_arr = false;
-						}
-						
-						if ( $char == '.' )
-						{
-							$buffer .= "']['";
+							$buffer .= "['";
 							$inside_arr = true;
 						}
 						else
 						{
-							$buffer .= $char;
+							if ( $inside_arr && in_array( $char, array( ';', '-', ',' ) ) )
+							{
+								$buffer .= "']";
+								$inside_arr = false;
+							}
+
+							if ( $char == '.' )
+							{
+								$buffer .= "']['";
+								$inside_arr = true;
+							}
+							else
+							{
+								$buffer .= $char;
+							}
 						}
 					}
+
+					if ( $inside_arr )
+					{
+						$buffer .= "']";
+					}
+
+					$var = $buffer;
 				}
-				
-				if ( $inside_arr )
-				{
-					$buffer .= "']";
-				}
-				
-				$var = $buffer;
-			}
-			
-			return $var;
-			
-		}, $view );
+
+				return $var;
+
+			}, $search );
+		}
+
+		return CCStr::replace( $view, $tags );
 	}
 }
