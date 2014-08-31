65de534ffeff1d7abc4030bc7d9d8ece127ab5e2
Mario Döring <deluna.yy@gmail.com>
2014-08-31 15:23:21 +0200

    Fix: CCAsset - #1
    Moved the asset holder to own class.
    Moved macros to static functions to make them php5.3 compatible and
    extensible.
    Added CCAsset unitests

--- src/classes/CCAsset.php
+++ src/classes/CCAsset.php
@@ -34,76 +34,109 @@ class CCAsset
 	protected static $_macros = array();
 	
 	/**
+	 * CSS Macro - Generates a css stylesheet link
+	 * 
+	 * @param string 			$uri
+	 * @param string 			$holder
+	 * @return string
+	 */
+	protected static function macro_css( $uri, $holder = null )
+	{
+		return '<link type="text/css" rel="stylesheet" href="'.static::uri( $uri, $holder ).'" />';
+	}
+	
+	/**
+	 * JS Macro - Generates a js source link
+	 * 
+	 * @param string 			$uri
+	 * @param string 			$holder
+	 * @return string
+	 */
+	protected static function macro_js( $uri, $holder = null )
+	{
+		return '<script type="text/javascript" src="'.static::uri( $uri, $holder ).'"></script>';
+	}
+	
+	/**
+	 * LESS Macro - Generates a less stylesheet link
+	 * 
+	 * @param string 			$uri
+	 * @param string 			$holder
+	 * @return string
+	 */
+	protected static function macro_less( $uri, $holder = null )
+	{
+		return '<link type="text/css" rel="stylesheet/less" href="'.static::uri( $uri, $holder ).'" />';
+	}
+	
+	/**
+	 * IMG Macro - Generates a less stylesheet link
+	 * 
+	 * @param string 			$uri
+	 * @param string 			$holder
+	 * @return string
+	 */
+	protected static function macro_img( $uri, $holder = null )
+	{
+		return '<img src="'.static::uri( $uri, $holder ).'" />';
+	}
+	
+	/**
+	 * Default Macro - Simply returns the given string
+	 * 
+	 * @param string 			$string
+	 * @return string
+	 */
+	protected static function macro__( $string )
+	{
+		return $string;
+	}
+	
+	/**
+	 * OG Macro - Generates open grapth tags
+	 *
+	 *     // <meta property="og:type" content="video" />
+	 *     CCAsset::og( 'type', 'video' );
+	 *     
+	 *     // <meta property="og:type" content="video" />
+	 *     // <meta property="og:res" content="1080p" />
+	 *     CCAsset::og( array( 'type' => 'video', 'res' => '1080p' ));
+	 * 
+	 * @param array|string 		$tags
+	 * @param string 			$content
+	 * @return string
+	 */
+	protected static function macro_og( $tags, $content = null ) 
+	{
+		if ( !is_array( $tags ) )
+		{
+			$tags = array( $tags => $content );
+		}
+		
+		$buffer = "";
+		
+		foreach( $tags as $key => $content )
+		{
+			$buffer .= '<meta property="og:'.$key.'" content="'.$content.'" />';
+		}
+		
+		return $buffer;
+	}
+	
+	/**
 	 * Register an assets macro 
 	 * 
-	 * @param string			$key
-	 * @param callback		$callback
+	 * @param string					$key
+	 * @param callback|string		$callback
 	 * @return void
 	 */
 	public static function macro( $key, $callback )
-	{
+	{	
 		static::$_macros[$key] = $callback; 
 	}
 	
 	/**
-	 * Static init
-	 * Load the inital macros
-	 *
-	 * @return void
-	 */
-	public static function _init()
-	{
-		// default tags
-		static::macro( '_', function( $tag ) 
-		{
-			return $tag;
-		});
-		
-		// stylesheets
-		static::macro( 'css', function( $file, $name = null ) 
-		{
-			return '<link type="text/css" rel="stylesheet" href="'.static::uri( $file, $name ).'" />';
-		});
-		
-		// js scripts
-		static::macro( 'js', function( $file, $name = null ) 
-		{
-			return '<script type="text/javascript" src="'.static::uri( $file, $name ).'"></script>';
-		});
-		
-		// less scripts
-		static::macro( 'less', function( $file, $name = null ) 
-		{
-			return '<link type="text/css" rel="stylesheet/less" href="'.static::uri( $file, $name ).'" />';
-		});
-		
-		// images
-		static::macro( 'img', function( $file, $name = null ) 
-		{
-			return '<img src="'.static::uri( $file, $name ).'" />';
-		});
-		
-		// open graph
-		static::macro( 'og', function( $tags, $content = null ) 
-		{
-			if ( !is_array( $tags ) )
-			{
-				$tags = array( $tags => $content );
-			}
-			
-			$buffer = "";
-			
-			foreach( $tags as $key => $content )
-			{
-				$buffer .= '<meta property="og:'.$key.'" content="'.$content.'" />';
-			}
-			
-			return $buffer;
-		});
-	}
-	
-	/**
-	 * check for macros and execute them
+	 * Check for macros and execute them
 	 *
 	 * @param string 	$name
 	 * @param array 		$arguments
@@ -113,38 +146,73 @@ class CCAsset
 	{
 		if ( !array_key_exists( $name, static::$_macros ) )
 		{
-			throw new \BadMethodCallException( "CCAsset::".$name." - No method or macro found." );
+			// we always check if the global class has that marco this way we
+			// can easly extend CCAsset default macros.
+			if ( !method_exists( '\\CCAsset', 'macro_'.$name ) )
+			{
+				throw new \BadMethodCallException( "CCAsset::".$name." - No method or macro found." );
+			}
+			
+			return call_user_func_array( array( '\\CCAsset', 'macro_'.$name ), $arguments );
+		}
+		
+		// if we have a string handle the macro as replacement pattern.
+		if ( is_string( static::$_macros[$name] ) )
+		{
+			// in this case argument 1 is going to be the uri 
+			// and argument 2 the asset holder
+			list( $uri, $holder ) = $arguments;
+			
+			return str_replace( ':uri', static::uri( $uri, $holder ), static::$_macros[$name] );
 		}
 		
 		return call_user_func_array( static::$_macros[$name], $arguments );
 	}
 
 	/** 
-	 * get the uri to an asset
+	 * Get the uri to an asset
 	 *
+	 *     // /assets/images/something.jpg
+	 *     CCAsset::uri( 'images/something.jpg' );
+	 *     
+	 *     // /assets/MyTheme/images/something.jpg
+	 *     CCAsset::uri( 'images/logo.png', 'theme' );
+	 *     
+	 *     // will not modify the given url
+	 *     CCAsset::uri( 'http://example.com/images/logo.jpg' );
+	 * 
 	 * @param string		$uri
 	 * @param string		$name 
 	 * @return string
 	 */
 	public static function uri( $uri, $name = null ) 
 	{
-		if ( strpos( $uri, '//' ) === false ) 
+		// when the uri conains a protocol we just return 
+		// the given uri.
+		if ( strpos( $uri, '://' ) === false ) 
 		{	
+			// If the path is relative and not absolute
+			// we prefix the holder path.
 			if ( substr( $uri, 0, 1 ) != '/' ) 
 			{
 				$uri = CCUrl::to( static::holder( $name )->path.$uri );
 			}
+			// When the given uri is absolute we remove the first
+			// slash and let CCUrl do the job.
 			else 
 			{
 				$uri = CCUrl::to( substr( $uri, 1 ) );
 			}
 		}
+		
 		return $uri;
 	}
 
 	/**
-	 * Get an asset holder
-	 * The assets are splittet by container aka holders you assign them a path.
+	 * Get an asset holder instance.
+	 *
+	 *     $holder = CCAsset::holder();
+	 *     $holder = CCAsset::holder( 'footer' );
 	 *
 	 * @param string			$name
 	 * @return CCAsset
@@ -158,111 +226,77 @@ class CCAsset
 		
 		if ( !isset( static::$instances[$name] ) ) 
 		{
-			 static::$instances[$name] = new static();
+			 static::$instances[$name] = new CCAsset_Holder();
 		}
 		
 		return static::$instances[$name];
 	}
 	
-	/** 
-	 * Add an asset to a holder.
-	 * This method checks the file extension to order the assets in the
-	 * container. 
-	 *
-	 * @param string			$item	This can be an .js .css file etc.
-	 * @param string 		$name
-	 * @return void
-	 */
-	public static function add( $item, $name = null ) 
-	{
-		// if its just a simple tag
-		if ( strpos( $item, "<" ) !== false ) 
-		{
-			static::holder( $name )->assets['_'][] = $item;
-		}
-		// a file path
-		else 
-		{
-			static::holder( $name )->assets[CCStr::extension( $item )][] = $item;
-		}
-	}
-	
 	/**
-	 * Get assets by type from an holder
+	 * Get all assets of a specific type in a holder and run them 
+	 * trough the maching macros.
 	 *
-	 * @param string		$extension
+	 *     CCAsset::code( 'css' );
+	 *     CCAsset::code( 'js', 'footer' ); 
+	 *
+	 * @param string		$type
 	 * @param string		$name
 	 */
-	public static function get( $extension = null, $name = null ) 
-	{
-		if ( is_null( $extension ) )
-		{
-			return static::holder( $name )->assets;
-		}
-		
-		$assets = static::holder( $name )->assets;
-		
-		if ( !array_key_exists( $extension, $assets ) )
-		{
-			return array();
-		}
-		
-		return $assets[$extension];
-	}
-	
-	/**
-	 * Get assets code by type from an holder
-	 *
-	 * @param string		$extension
-	 * @param string		$name
-	 */
-	public static function code( $extension = null, $name = null ) 
+	public static function code( $type, $name = null ) 
 	{
 		$buffer = "";
 		
-		foreach( static::get( $extension, $name ) as $item )
+		foreach( static::holder( $name )->get( $type ) as $item )
 		{
-			$buffer .= call_user_func( 'CCAsset::'.$extension, $item, $name );
+			$buffer .= call_user_func( array( '\\CCAsset', $type ), $item, $name );
 		}
 		
 		return $buffer;
 	}
 	
-	/**
-	 * Clear an asset holder
-	 * Delete all containing assets
-	 * 
-	 * @param string 		$ext
-	 * @param string			$name
+	/** 
+	 * Add an asset to the holder. Basically this method checks the 
+	 * file extension to sort them and generate the correct code using the macros.
+	 *
+	 *     CCAsset::add( 'jquery.js' );
+	 *     CCAsset::add( 'style.css' );
+	 *     CCAsset::add( '<script>document.write( "Hello World" );</script>', 'footer' );
+	 *
+	 * @param string			$item
 	 * @return void
 	 */
-	public static function clear( $ext, $name = null ) 
+	public static function add( $item, $name = null ) 
 	{
-		static::holder( $name )->assets[$ext][] = array();
-	}
-		
-	/**
-	 * get the entire output from an asset holder
-	 *
-	 * @param string 		$name
-	 * @return string
-	 */
-	public static function all( $name = null ) 
-	{
-		return static::code( '_', $name ).static::code( 'css', $name ).static::code( 'js', $name );
+		return static::holder( $name )->add( $item );
 	}
 	
 	/**
-	 * path modifier
+	 * Get all assets by a specific macro / type
 	 *
-	 * @var string
+	 *     CCAsset::get();
+	 *     CCAsset::get( 'css', 'theme' );
+	 *
+	 * @param string			$type		By passing null all items from all types are returned
+	 * @return array
 	 */
-	public $path = 'assets/';
+	public static function get( $type = null, $name = null ) 
+	{
+		return static::holder( $name )->get( $type );
+	}
 	
 	/**
-	 * Content
+	 * Clear the asset holder, deletes all contained assets 
+	 * of a special type or if $type is null everything.
 	 *
-	 * @var array
+	 *     CCAsset::clear();
+	 *     CCAsset::clear( 'css' );
+	 *     CCAsset::clear( 'js', 'footer' );
+	 * 
+	 * @param string 		$type
+	 * @return void
 	 */
-	public $assets = array();
+	public static function clear( $type, $name = null ) 
+	{
+		return static::holder( $name )->clear( $type );
+	}	
 }
--- tests/Core/CCAsset.php
+++ tests/Core/CCAsset.php
@@ -8,7 +8,177 @@
  * @version		2.0
  * @copyright 	2010 - 2014 ClanCats GmbH
  *
+ * @group Core
+ * @group CCAsset
  */
-/*class CCAsset_Test extends \PHPUnit_Framework_TestCase
+class CCAsset_Test extends \PHPUnit_Framework_TestCase
 {
-}*/
+	/**
+	 * CCAsset::uri tests
+	 */
+	public function test_uri()
+	{
+		$this->assertEquals( '/assets/images/foo.jpg', CCAsset::uri( 'images/foo.jpg' ) );
+		
+		// lets define a holder
+		CCAsset::holder( 'phpunit' )->path = "phpunit/images/";
+		
+		$this->assertEquals( '/phpunit/images/foo.jpg', CCAsset::uri( 'foo.jpg', 'phpunit' ) );
+		
+		// url
+		$this->assertEquals( 'http://foo.bar', CCAsset::uri( 'http://foo.bar' ) );
+		$this->assertEquals( 'http://foo.bar', CCAsset::uri( 'http://foo.bar', 'phpunit' ) );
+		
+		// absolute path
+		$this->assertEquals( '/foo/bar', CCAsset::uri( '/foo/bar' ) );
+		$this->assertEquals( '/foo/bar', CCAsset::uri( '/foo/bar', 'phpunit' ) );
+	}
+	
+	/**
+	 * CCAsset::og tests
+	 */
+	public function test_open_graph()
+	{
+		$this->assertEquals( '<meta property="og:type" content="video" />', CCAsset::og( 'type', 'video' ) );
+		
+		// multiple
+		$this->assertEquals( '<meta property="og:type" content="image" /><meta property="og:size" content="512px" />', CCAsset::og( array(
+			'type' => 'image',
+			'size' => '512px',
+		)) );
+	}
+	
+	/**
+	 * CCAsset::css tests
+	 */
+	public function test_css()
+	{
+		$this->assertEquals( '<link type="text/css" rel="stylesheet" href="/assets/foo.css" />', CCAsset::css( 'foo.css' ) );
+	}
+	
+	/**
+	 * CCAsset::js tests
+	 */
+	public function test_js()
+	{
+		$this->assertEquals( '<script type="text/javascript" src="/assets/foo.js"></script>', CCAsset::js( 'foo.js' ) );
+	}
+	
+	/**
+	 * CCAsset::less tests
+	 */
+	public function test_less()
+	{
+		$this->assertEquals( '<link type="text/css" rel="stylesheet/less" href="/assets/foo.less" />', CCAsset::less( 'foo.less' ) );
+		
+		// lets define a holder
+		CCAsset::holder( 'phpunit-less' )->path = "assets/phpunit/less/";
+		
+		$this->assertEquals( '<link type="text/css" rel="stylesheet/less" href="/assets/phpunit/less/foo.less" />', CCAsset::less( 'foo.less', 'phpunit-less' ) );
+	}
+	
+	/**
+	 * CCAsset::img tests
+	 */
+	public function test_img()
+	{
+		$this->assertEquals( '<img src="/assets/logo.png" />', CCAsset::img( 'logo.png' ) );
+	}
+	
+	/**
+	 * CCAsset invalid macro test
+	 *
+	 * @expectedException BadMethodCallException
+	 */
+	public function test_bad_macro()
+	{
+		CCAsset::this_does_not_exist();
+	}
+	
+	/**
+	 * CCAsset::uri tests
+	 */
+	public function test_macro()
+	{
+		$this->assertEquals( 'foo', CCAsset::_( 'foo' ) );
+		
+		// create new macro
+		CCAsset::macro( 'phpunit', function( $to ) 
+		{
+			return 'Hello '.$to;
+		});
+		
+		$this->assertEquals( 'Hello Spock', CCAsset::phpunit( 'Spock' ) );
+		
+		// file macro
+		CCAsset::macro( 'bg', '<div style="background-image: url(:uri);"></div>' );
+		
+		CCAsset::holder( 'phpunit-images' )->path = "assets/phpunit/images/";
+		
+		$expected = '<div style="background-image: url(/assets/phpunit/images/background.jpg);"></div>';
+		
+		$this->assertEquals( $expected, CCAsset::bg( 'background.jpg', 'phpunit-images' ) );
+	}
+	
+	/**
+	 * CCAsset::add, get & clear tests
+	 */
+	public function test_add_get_clear()
+	{
+		$assets = array(
+			'style.css',
+			'bootstrap.css',
+			'application.js',
+			'jquery.js',
+			'underscore.js',
+		);
+		
+		foreach( $assets as $asset )
+		{
+			CCAsset::add( $asset );
+			CCAsset::add( $asset, 'second' );
+		}
+		
+		$this->assertEquals( 2, count( CCAsset::get() ) );
+		$this->assertEquals( 2, count( CCAsset::get( null, 'second' ) ) );
+		$this->assertEquals( 0, count( CCAsset::get( 'foo' ) ) );
+		
+		$this->assertEquals( 3, count( CCAsset::get( 'js' ) ) );
+		$this->assertEquals( 3, count( CCAsset::get( 'js', 'second' ) ) );
+		$this->assertEquals( 0, count( CCAsset::get( 'js', 'footer' ) ) );
+		
+		$this->assertEquals( 2, count( CCAsset::get( 'css' ) ) );
+		$this->assertEquals( 2, count( CCAsset::get( 'css', 'second' ) ) );
+		$this->assertEquals( 0, count( CCAsset::get( 'css', 'other' ) ) );
+		
+		CCAsset::clear( 'js' );
+		
+		$this->assertEquals( 0, count( CCAsset::get( 'js' ) ) );
+		$this->assertEquals( 3, count( CCAsset::get( 'js', 'second' ) ) );
+		
+		CCAsset::clear();
+		
+		$this->assertEquals( 0, count( CCAsset::get() ) );
+		$this->assertEquals( 2, count( CCAsset::get( null, 'second' ) ) );
+		
+		// test adding plain tag
+		CCAsset::add( '<style></style>' );
+		
+		$this->assertEquals( 1, count( CCAsset::get( '_' ) ) );
+	}
+	
+	/**
+	 * CCAsset::code tests
+	 */
+	public function test_code_gen()
+	{	
+		CCAsset::macro( 'test', '<:uri>' );
+		
+		CCAsset::holder( 'root' )->path = '/';
+		
+		CCAsset::add( 'foo.test', 'root' );
+		CCAsset::add( 'bar.test', 'root' );
+		
+		$this->assertEquals( '</foo.test></bar.test>', CCAsset::code( 'test', 'root' ) );
+	}
+}
