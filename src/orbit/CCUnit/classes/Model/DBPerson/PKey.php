105f269a5fcb855e884d1d53007dfcf6997f217a
Mario Döring <deluna.yy@gmail.com>
2015-01-01 12:12:57 +0100

    Header update 2015
    Happy new year!

--- src/orbit/CCUnit/classes/Model/DBPerson/PKey.php
+++ src/orbit/CCUnit/classes/Model/DBPerson/PKey.php
@@ -5,7 +5,7 @@
  * @package       CCUnit
  * @author        Mario Döring <mario@clancats.com>
  * @version       1.0.0
- * @copyright     2010 - 2014 ClanCats GmbH
+ * @copyright     2010 - 2015 ClanCats GmbH
  */
 class Model_DBPerson_PKey extends \DB\Model
 {
